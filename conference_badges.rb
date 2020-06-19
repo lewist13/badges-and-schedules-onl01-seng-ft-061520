6a18499d8030207bda8019797fb70b42dacc7eb9
TaeVon Lewis <lewistpro@gmail.com>
2020-06-19 16:24:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 def batch_badge_creator(attendees)
   conference_badges = []
   attendees.each do |name|
-    conference_badges.push("Hello, my name is #{name}.")
+    conference_badges.push("Hello, my name is #{conference_badges}.")
   end
   conference_badges
 end
